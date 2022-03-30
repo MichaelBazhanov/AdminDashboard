7a4fda50ec6c4f6d83acd8e956e23334d0a6abb5
michael bazhanov <michaelbazhanov33@gmail.com>
2022-03-30 13:49:03 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
     ],
   },
   plugins: [
-    new FaviconsWebpackPlugin('./src/asset/images/Svelte.jpg'), // svg works too!
+    // new FaviconsWebpackPlugin('./src/asset/images/Svelte.jpg'), // svg works too!
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
     }),
